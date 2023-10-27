4245a90c5ae01edc10176aad9043e036b7e16476
CodeMajkell <149201330+CodeMajkell@users.noreply.github.com>
2023-10-27 22:20:08 +0200

    Add Zadanie 1

---
+++ Lab1/Zadanie 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Michal Kujawa"<< endl;
+    cout << "s26408"<< endl;
+    cout << "Polsko-Japonska Akademia Technik Komputerowych"<< endl;
+
+
+    return 0;
+}
